bb272b22e73912219c75dd3d57d28a8c7f9e3981
alexFlores9 <61833923+alexFlores9@users.noreply.github.com>
2020-05-14 17:07:42 -0600

    Create prueba2.php

---
+++ prueba2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Felicidades, vamos por un buen camino</h2>
+
+    <h5>estamos aprendiendo</h5>
+</body>
+</html>
