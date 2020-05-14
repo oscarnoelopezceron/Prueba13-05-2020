f4437581841453222b68e9855d832e44790c626f
oscarnoelopezceron <61834003+oscarnoelopezceron@users.noreply.github.com>
2020-05-13 22:48:29 -0600

    Create index.php
    No se que estoy haciendo XD

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Inicio</title>
+</head>
+<body>
+<form action="post">
+<h1>Hola múndo:</h1>
+	<label for="usuario">Usuario: </label>
+	<input type="text">
+	<label for="">Contraseña: </label>
+	<input id="pasword" type="pasword">
+</form>
+<?php
+if (condition) {
+	# code...
+}
+
+
+
+?>
+
+<h1>Holamundo</h1>
+	
+</body>
+</html>
